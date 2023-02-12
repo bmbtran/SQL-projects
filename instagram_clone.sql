ed8a9972580e5a4de6f9b94d274b1f45ea80921d
Tran Bui <103015816+bmbtran@users.noreply.github.com>
2023-02-11 23:25:23 -0500

    Update instagram_clone.sql
    add tables: follows, tags, and a junction table: photos -tags

--- instagram_clone.sql
+++ instagram_clone.sql
@@ -35,3 +35,30 @@ CREATE TABLE likes(
 	FOREIGN KEY(photo_id) REFERENCES photos(id),
 	PRIMARY KEY(user_id,photo_id)
 );
+
+
+/*follows*/
+CREATE TABLE follows(
+	follower_id INT NOT NULL,
+	followee_id INT NOT NULL,
+	created_at TIMESTAMP DEFAULT NOW(),
+	FOREIGN KEY (follower_id) REFERENCES users(id),
+	FOREIGN KEY (followee_id) REFERENCES users(id),
+	PRIMARY KEY(follower_id,followee_id)
+);
+
+/*Tags*/
+CREATE TABLE tags(
+	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+	tag_name VARCHAR(255) UNIQUE NOT NULL,
+	created_at TIMESTAMP DEFAULT NOW()
+);
+
+/*junction table: Photos - Tags*/
+CREATE TABLE photo_tags(
+	photo_id INT NOT NULL,
+	tag_id INT NOT NULL,
+	FOREIGN KEY(photo_id) REFERENCES photos(id),
+	FOREIGN KEY(tag_id) REFERENCES tags(id),
+	PRIMARY KEY(photo_id,tag_id)
+);
