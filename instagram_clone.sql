040df5d6210cb7b3b79480e3f2b8ad9c76aa78e5
Tran Bui <103015816+bmbtran@users.noreply.github.com>
2023-02-11 23:23:13 -0500

    Update instagram_clone.sql
    add following tables: photos, comments, likes

--- instagram_clone.sql
+++ instagram_clone.sql
@@ -5,3 +5,33 @@ CREATE TABLE users(
 	id INT AUTO_INCREMENT UNIQUE PRIMARY KEY,
 	username VARCHAR(255) NOT NULL,
 	created_at TIMESTAMP DEFAULT NOW()
+);
+/*Photos*/
+CREATE TABLE photos(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	image_url VARCHAR(355) NOT NULL,
+	user_id INT NOT NULL,
+	created_dat TIMESTAMP DEFAULT NOW(),
+	FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+/*Comments*/
+CREATE TABLE comments(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	comment_text VARCHAR(255) NOT NULL,
+	user_id INT NOT NULL,
+	photo_id INT NOT NULL,
+	created_at TIMESTAMP DEFAULT NOW(),
+	FOREIGN KEY(user_id) REFERENCES users(id),
+	FOREIGN KEY(photo_id) REFERENCES photos(id)
+);
+
+/*Likes*/
+CREATE TABLE likes(
+	user_id INT NOT NULL,
+	photo_id INT NOT NULL,
+	created_at TIMESTAMP DEFAULT NOW(),
+	FOREIGN KEY(user_id) REFERENCES users(id),
+	FOREIGN KEY(photo_id) REFERENCES photos(id),
+	PRIMARY KEY(user_id,photo_id)
+);
